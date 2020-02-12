ff1f900fffc54b953722dcef5697b809c8fdbe6e
Emira_Harrioui <harrioui.e@gmail.com>
2020-02-12 12:05:59 +0100

    create db, confConnection

---
+++ BDD/conf.sql
@@ -0,0 +1,9 @@
+#creation BDD hotel
+CREATEDB hotel
+PSQL hotel
+
+
+
+CREATE TABLE conferencier(id SERIAL PRIMARY KEY NOT NULL, firstname VARCHAR(20) NOT NULL, name VARCHAR(20) NOT NULL, description VARCHAR(250), profession VARCHAR(250) NOT NULL, status_actif BOOL NOT NULL);
+
+CREATE TABLE conference(id SERIAL PRIMARY KEY NOT NULL, title VARCHAR(50) NOT NULL, resume VARCHAR(250) , date DATE NOT NULL, hour TIME NOT NULL,date_creation DATE NOT NULL, id_conferencier INTEGER NOT NULL);
